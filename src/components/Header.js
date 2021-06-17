c6c1cc9599c66492e18890dbed4fc5b0c4cd1ab9
jamesncox <jamesncox@yahoo.com>
2021-06-17 14:29:13 -0500

    Create Secrets header and no one will know subheader

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header-wrapper">
+      <h1 className="header">SECRETS</h1>
+      <h2 className="sub-header">No one will know</h2>
+    </header>
+  );
+}
