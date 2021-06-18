a17e1307b943ade4698472d94efea82aee6edb37
jamesncox <jamesncox@yahoo.com>
2021-06-18 08:59:07 -0500

    Add Enter ID form to front box

--- src/components/RoomPortal.js
+++ src/components/RoomPortal.js
@@ -1,4 +1,5 @@
-import { Button } from "antd";
+import { Button, Input } from "antd";
+import { RightOutlined } from "@ant-design/icons";
 
 export default function RoomPortal() {
   return (
@@ -6,12 +7,25 @@ export default function RoomPortal() {
       <div className="box-back">
         <h3 className="rooms-title">Rooms</h3>
       </div>
+
       <div className="box-front">
+        {/* <div className="front-content-wrapper"> */}
         <Button type="primary" className="create-btn">
           Create
         </Button>
         <p className="or-text">OR</p>
+
+        <form name="basic" className="id-form">
+          <div className="form-wrapper">
+            <Input placeholder="Enter ID" className="id-input" />
+
+            <Button type="primary" htmlType="submit" className="id-btn">
+              <RightOutlined />
+            </Button>
+          </div>
+        </form>
       </div>
+      {/* </div> */}
     </>
   );
 }
