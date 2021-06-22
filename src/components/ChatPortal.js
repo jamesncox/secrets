701b9a69c644525976862339b0cd6ac633caf8bc
jamesncox <jamesncox@yahoo.com>
2021-06-21 22:19:19 -0500

    Refactor ChatPortal with one, semi-opaque box, with forms and buttons outside the box

--- src/components/ChatPortal.js
+++ src/components/ChatPortal.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 export default function ChatPortal() {
+  const [input, setInput] = useState("");
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <div className="chat-portal-wrapper">
-      <div className="box-back-chat">
-        <button className="show-id-btn">Room ID</button>
-      </div>
+      <button className="show-id-btn">Room ID</button>
+      <div className="box-back-chat"></div>
+      <form name="basic" className="message-form">
+        <div className="input-wrapper">
+          <input
+            placeholder="Your message..."
+            className="id-input"
+            value={input}
+            onChange={handleOnChange}
+          />
 
-      <div className="box-front-chat"></div>
+          <button className="send-btn" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
